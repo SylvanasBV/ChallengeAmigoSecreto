43d9c1ce437235847bf2c69ab9efaadf484281a3
SylvanasBV <andres7.7mac@gmail.com>
2025-03-18 16:48:15 -0500

    finalizado el primer entregable con todas las actividades

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 class Sorteo {
+
+    
     constructor(){
-        this.amigo = [];
+        this.friend = [];
+        this.friendSelected = [];
     }
 
     validarNombre(name){
@@ -12,16 +15,16 @@ class Sorteo {
             alert('Por favor, ingrese un nombre válido, no solo números')
             return;
         }
-        this.amigo.push(name);
+        this.friend.push(name);
         this.actualizarAmigo();
-        console.log(this.amigo);
+        console.log(this.friend);
     }
 
     actualizarAmigo (){
         let listaAmigos = document.querySelector('#listaAmigos');
         listaAmigos.innerHTML = "";
 
-        this.amigo.forEach((nameFriend)=>{
+        this.friend.forEach((nameFriend)=>{
             let li = document.createElement('li');
             li.textContent = nameFriend;
             listaAmigos.appendChild(li)
@@ -31,13 +34,44 @@ class Sorteo {
 
     sortearAmigo () {
 
-        if(this.amigo.length === 0){
+        if(this.friend.length === 0){
             alert('No hay amigos en la lista para sortear');
             return null;
         }
-        
-        let randomFriend = Math.floor(Math.random()*this.amigo.length);
-        return this.amigo[randomFriend];
+        if (this.friendSelected.length !== this.friend.length) {
+            let randomFriend = Math.floor(Math.random()*this.friend.length);
+            if (this.friendSelected.includes(this.friend[randomFriend])){
+                return this.sortearAmigo()
+            }
+            else{
+
+
+                this.friendSelected.push(this.friend[randomFriend]);
+                let selectList = document.querySelector('#resultado');
+                let listFriends = document.querySelector('#listaAmigos');
+                let drawButton = document.querySelector('.button-draw');
+                
+                selectList.style.display = 'block';
+                listFriends.style.display = 'none';
+                selectList.innerHTML = '';
+                drawButton.setAttribute('disabled', true);
+
+                let li = document.createElement('li');
+                li.textContent = `El amigo sorteado es: ${this.friend[randomFriend]}`;
+                selectList.appendChild(li);
+                console.log("Seleccionado:", li); // Mostrar cada <li> agregado
+
+                setTimeout(() => {
+                    listFriends.style.display = 'block';
+                    drawButton.removeAttribute('disabled'); // Habilita el botón de nuevo
+                    selectList.style.display = 'none';
+
+
+                },3000)
+            }}
+        else {
+            return alert('Todos los amigos han sido sorteados');
+        }
     }
 }
 
@@ -55,7 +89,7 @@ document.querySelector('.button-draw').addEventListener('click',
     function escogerAmigo(){
         let ganador = sorteo.sortearAmigo();
         if (ganador){
-            alert(`El amugo sorteado es: ${ganador}`);
+            alert(`El amigo sorteado es: ${ganador}`);
         }
     }
 )
